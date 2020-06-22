4570fa6351f05528cadc6971ba6ab1d7b6b956dd
PKr <p.krebs@oekotec.de>
2020-06-22 16:35:41 +0200

    Update auswertung2.py

--- auswertung2.py
+++ auswertung2.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import sqlite3
 from dbhelper import getTableSelect
 import datetime as dt
 from matplotlib.pyplot import *
 
 print('Read database...')
-#rec = getTableSelect("SELECT w.timestmp,w.timestampUNX,w.city,w.temperature,w.timestampUNX-tss.timestampUNX FROM Wetter w,Timestampsolver tss WHERE ABS(w.timestampUNX - tss.timestampUNX) < 900 AND (w.city LIKE ('%Mich%') OR w.city LIKE ('%Schoe%') OR w.city LIKE ('%Belz%')) ORDER BY w.timestampUNX") # AND (w.city LIKE ('%Mich%') OR w.city LIKE ('%Schoe%')) 
-rec = getTableSelect("SELECT w.timestmp,w.timestampUNX,w.city,w.temperature FROM Wetter w WHERE w.city LIKE ('%Mich%') OR w.city LIKE ('%Schoe%') OR w.city LIKE ('%Belz%') ORDER BY w.timestampUNX") # AND (w.city LIKE ('%Mich%') OR w.city LIKE ('%Schoe%')) 
-print('Create dataframe')
-data = pd.DataFrame.from_records(rec)
+
+#rec = getTableSelect("SELECT w.timestmp,w.timestampUNX,w.city,w.temperature FROM Wetter w WHERE w.city LIKE ('%Mich%') OR w.city LIKE ('%Schoe%') OR w.city LIKE ('%Belz%') ORDER BY w.timestampUNX") # AND (w.city LIKE ('%Mich%') OR w.city LIKE ('%Schoe%')) 
+#print('Create dataframe')
+#data = pd.DataFrame.from_records(rec)
+
+data = pd.read_sql_query("SELECT w.timestmp,w.timestampUNX,w.city,w.temperature FROM Wetter w WHERE w.city LIKE ('%Mich%') OR w.city LIKE ('%Schoe%') OR w.city LIKE ('%Belz%') ORDER BY w.timestampUNX",sqlite3.connect('wetter.db'))
 data.columns = ['Zeitstempel', 'Unix Zeitstempel', 'city', 'Temperatur']
 data['ts'] = data['Zeitstempel'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
 
